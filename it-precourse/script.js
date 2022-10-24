ecc9063b5d7c6508697d6c4d11148cf8e3e7468f
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-10-24 16:54:28 +0700

    Add watch() function to JS

--- it-precourse/script.js
+++ it-precourse/script.js
@@ -1,10 +1,20 @@
 var showing = "empty";
 function show(val) {
     document.getElementById(showing.toString()).style.visibility = "hidden";
+    document.getElementById("episodes1").style.visibility = "hidden";
+    document.getElementById("episodes2").style.visibility = "hidden";
+    document.getElementById("episodes3").style.visibility = "hidden";
     if(showing == val) {
         showing = "empty";
     } else {
         showing = val;
     }
     document.getElementById(showing.toString()).style.visibility = "visible";
+}
+
+function watch(s, e) {
+    document.getElementById("episodes" + s.toString()).style.visibility = "visible";
+    url = "video/s" + s.toString() + "/e" + e.toString() + ".mp4";
+    document.getElementById("video_src").src = url;
+    document.getElementById("video").load();
 }
